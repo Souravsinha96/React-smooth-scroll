43b93b84a91b0e8a27257a9743e78f40c3e3dced
Souravsinha96 <souravsinha1604@gmail.com>
2021-02-21 21:17:43 +0530

    Completed Infosection component

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,59 @@
+import svg1 from "../../images/svg-1.svg";
+import svg2 from "../../images/svg-2.svg";
+import svg3 from "../../images/svg-3.svg";
+import svg4 from "../../images/svg-4.svg";
+import svg5 from "../../images/svg-5.svg";
+import svg6 from "../../images/svg-6.svg";
+
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Easy free banking for entrepreneurs",
+  description:
+    "Get the financial tools and insights to start, build, and grow your business.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: svg4,
+  alt: "bank",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "unlimited Access",
+  headline: "Login to your account at any time",
+  description:
+    "We have you covered no matter where you are loacted.All you need is an internet connection and a bank account",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: svg2,
+  alt: "piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join Now",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Get the financial tools and insights to start, build, and grow your business.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: svg5,
+  alt: "paper",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
